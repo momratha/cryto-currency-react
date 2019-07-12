d11c39a50836fa965788a47ed22dcc79a175b4ae
Ratha Mom <momratha@gmail.com>
2019-07-12 21:56:38 +1000

    add requirment.png

--- src/components/Cryto/Row.js
+++ src/components/Cryto/Row.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import moment from "moment";
 const Row = ({ cryto }) => {
+  const reduceWhich = (arr, comparator = (a, b) => a - b) =>
+    arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
+  const min = reduceWhich(cryto.quotes);
+  const max = reduceWhich(cryto.quotes, (a, b) => b.price - a.price);
   return (
     <tr>
+      <td scope="row">{moment(cryto.date).format("DD-MMMM-YYYY")}</td>
       <td>{cryto.currency}</td>
-      {cryto.quotes}
+      <td>
+        ${min.price} at {moment(min.time, "hmm").format("LT")}
+      </td>
+      <td>
+        ${max.price} at {moment(max.time, "hmm").format("LT")}
+      </td>
+      <td>${(max.price - min.price).toFixed(2)}</td>
     </tr>
   );
 };
--- src/components/Cryto/Table.js
+++ src/components/Cryto/Table.js
@@ -2,10 +2,19 @@ import React from "react";
 import Row from "./Row";
 const Table = ({ crytos }) => {
   return (
-    <table>
-      {crytos.map((item, i) => {
-        return <Row cryto={item} />;
-      })}
+    <table className="table">
+      <tr>
+        <th scope="col">Date</th>
+        <th scope="col">Currency</th>
+        <th scope="col">Buy</th>
+        <th scope="col">Sell</th>
+        <th scope="col">Profit</th>
+      </tr>
+      <tbody>
+        {crytos.map(item => {
+          return <Row cryto={item} />;
+        })}
+      </tbody>
     </table>
   );
 };
